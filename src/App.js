d54055e2f93ebc69d1854f4cf3cee10e791f29a9
Shannon Melvin <Shannonmelvin88@gmail.com>
2023-04-05 18:14:14 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
+import React, {useState} from 'react';
 
-import './App.css';
-import TodoForm from './components/TodoForm';
 
-import Home from './Home';
+function App() {
 
-import React, { Component } from 'react'
+  const [list,setList] = useState([]);
+  const [input,setInput] = useState('');
 
-export default class App extends Component {
-  render() {
+  const addTodo =(todo) => {
+    const newTodo = {
+      id:Math.random(),
+      todo:todo,
+    }
+  
+
+    // add the todo to the list 
+  
+    setList([...list, newTodo]); 
+
+    // clear input box  
     
-
-    return (  
-  <div className='App'>
-      <Home/>
-      <TodoForm/>
-
-  </div>
+    setInput(''); 
+  };
     
-   
-    
-    )
-  }
+    const deleteTodo = (id) => {
+      // filter out to do with the id 
+      const newList = list.filter((todo) => todo.id !== id);
+      setList(newList);
+  
+  };
+  
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type ="text" value = {input} onChange={(event)=> setInput(event.target.value)}/>
+      <button onClick = {()=> addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo)=> (
+        <li key = {todo.id}>
+            {todo.todo}
+            <button onClick = {()=> deleteTodo(todo.id)}>&times;</button>
+        </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-
-
+export default App;
